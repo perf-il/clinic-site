dd40ea89780fd873a674350e5fac688d466cd5ab
Ilia Perfilov <ind.climb@gmail.com>
2023-09-22 21:35:36 +0300

    fixed settings

--- config/settings.py
+++ config/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-if DEBUG:
+ENV_TYPE = os.getenv('ENV_TYPE')
+if ENV_TYPE == 'local':
     STATICFILES_DIRS = (
         BASE_DIR / 'static',
     )
